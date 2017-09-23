86233c9c6ff3180c1ba1bfd230e530e755d3bac4
phagon <tuongpv@hotmail.com>
2017-09-23 17:26:31 +0700

    Add directive

--- wwwroot/Client/Controllers/newsController.js
+++ wwwroot/Client/Controllers/newsController.js
@@ -1,17 +1,35 @@
 (function () {
     'use strict'
 
-    var newsApp = angular.module('adminApp.app.news', []);
-    
+    var newsApp = angular.module('adminApp.app.news', ['ui.bootstrap']);
 
-    newsApp.controller('newsCtrl', function ($scope, $http) {
+
+    newsApp.controller('newsCtrl', function ($scope, $http, $uibModal ) {
         $scope.title = "This is title of post";
         $scope.approved = true;
 
+        $scope.openModal = function (size) {
+            //
+            BootstrapDialog.alert('I want banana!');
 
-        $scope.showDialog = function(){
-            alert("a");
-        }
+            //
+            // var modalInstance = $uibModal.open({
+            //     template: "<div>This is modal. It's show when click</div>",
+            //     size: size,
+            //     resolve: {
+            //         items: function () {
+            //             return $scope.items;
+            //         }
+            //     }
+            // });
+
+            // modalInstance.result.then(function (selectedItem) {
+            //     $scope.selected = selectedItem;
+            // }, function () {
+
+            // });
+        };
+
 
         $scope.save = function () {
             $http.post('api/news').success(function (data, status, headers, config) {
@@ -27,8 +45,7 @@
 
                 }).error(function (data, status, headers, config) {
 
-            });
+                });
         }
     });
-
 })();
